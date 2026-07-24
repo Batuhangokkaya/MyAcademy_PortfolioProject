87203059975f06bca4ec19e4e04b50654dbf230d
Batuhangokkaya <batuhangkk34@gmail.com>
2026-07-25 01:10:19 +0300

    Project update

---
+++ Portfolio/Controllers/ProjectTechnologiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Data.Context;
+
+namespace Portfolio.Controllers
+{
+    public class ProjectTechnologiesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProjectTechnologiesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _context.ProjectTechStacks.ToList();
+            return View(values);
+        }
+    }
+}
